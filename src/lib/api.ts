66db70235e633a4a3d5a1c6b182dc6c8185aaedc
Vikram Hegde <vikramhegde4037@gmail.com>
2026-03-25 18:00:31 +0530

    Added backup api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,15 +1,52 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const PRIMARY_API = import.meta.env.VITE_API_URL;
+const BACKUP_API = import.meta.env.VITE_BACKUP_API_URL;
+
+let ACTIVE_BASE_URL = PRIMARY_API;
+
+const checkServer = async (url: string) => {
+    try {
+        let finalUrl = url;
+        if (url.endsWith('/api/v1'))
+            finalUrl = url.split('/api/v1')[0];
+        await axios.get(finalUrl, { timeout: 3000 });
+        return true;
+    } catch {
+        return false;
+    }
+};
+
+export const initApiBase = async () => {
+    const isPrimaryUp = await checkServer(PRIMARY_API);
+
+    if (isPrimaryUp) {
+        ACTIVE_BASE_URL = PRIMARY_API;
+        console.log("✅ Using PRIMARY backend");
+    } else {
+        const isBackupUp = await checkServer(BACKUP_API);
+
+        if (isBackupUp) {
+            ACTIVE_BASE_URL = BACKUP_API;
+            console.log("⚠️ Primary down, switched to BACKUP");
+        } else {
+            console.error("❌ Both backends are down");
+        }
+    }
+
+    // Update axios instances dynamically
+    api.defaults.baseURL = ACTIVE_BASE_URL;
+    apiPrivate.defaults.baseURL = ACTIVE_BASE_URL;
+};
 
 export const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: ACTIVE_BASE_URL,
     withCredentials: true,
     timeout: 10000,
 });
 
 export const apiPrivate = axios.create({
-    baseURL: BASE_URL,
+    baseURL: ACTIVE_BASE_URL,
     withCredentials: true,
     timeout: 10000,
 });
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.tsx';
+import { initApiBase } from './lib/api.ts';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+initApiBase().finally(() => {
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </StrictMode>,
+  )
+});
